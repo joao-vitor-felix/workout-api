177b11a38f91425e78ccacd71b47eb07b8fe37f6
João Vitor <joaovitorfelixcontato@gmail.com>
2025-08-01 19:51:39 -0300

    fix: handle nil data case in WriteJSON function

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -12,6 +12,11 @@ import (
 type Envelope map[string]any
 
 func WriteJSON(w http.ResponseWriter, status int, data Envelope) error {
+	if data == nil {
+		w.WriteHeader(status)
+		return nil
+	}
+
 	jsonData, err := json.MarshalIndent(data, "", " ")
 	if err != nil {
 		http.Error(w, "Failed to marshal JSON", http.StatusInternalServerError)
